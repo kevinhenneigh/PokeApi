6aa0ee62c17e1c6c47e6f1c361f79fa35dd20a25
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-10-25 14:27:47 -0700

    Created basic API Client to retrieve Pokemon by name

---
+++ PokeApiConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using PokeApiCore;
+using System;
+using System.Threading.Tasks;
+
+namespace PokeApiConsole
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            PokeApiClient client = new PokeApiClient();
+            string result = await client.GetPokemonByName("bulbasaur");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PokeApiCore/PokeApiClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PokeApiCore
+{
+    /// <summary>
+    /// Cleint class to consume PokeAPI
+    /// https://pokeapi.co/
+    /// </summary>
+    public class PokeApiClient
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// Retrieve Pokemon by Name
+        /// </summary>
+        /// <exception cref="HttpRequestException"></exception>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task<string> GetPokemonByName(string name)
+        {
+            try
+            {
+                string url = $"https://pokeapi.co/api/v2/pokemon/{name}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                throw ex;
+            }
+        }
+
+        public void GetPokemonById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
