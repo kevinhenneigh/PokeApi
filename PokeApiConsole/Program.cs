514f365c152f72a974e793d6837a1dc9d484f557
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-10-26 16:41:35 -0700

    refactor Pokemon get by name

--- PokeApiConsole/Program.cs
+++ PokeApiConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using PokeApiCore;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PokeApiConsole
@@ -9,12 +10,26 @@ namespace PokeApiConsole
         static async Task Main()
         {
             PokeApiClient client = new PokeApiClient();
-            Pokemon result = await client.GetPokemonByName("bulbasaur");
+            try
+            {
+                Pokemon result = await client.GetPokemonByName("bulbasaur");
 
-            Console.WriteLine($"Pokemon Id: {result.id}" +
+                Console.WriteLine($"Pokemon Id: {result.id}" +
                 $"\nName: {result.name}" +
                 $"\nWeight (in pounds): {result.weight}" +
                 $"\nHeight (in inches): {result.height}");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("I'm sorry, that Pokemon does not exist");
+            }
+            catch (HttpRequestException)
+            {
+                Console.WriteLine("Please try again later");
+            }
+            
+
+            
 
             Console.ReadKey();
         }
