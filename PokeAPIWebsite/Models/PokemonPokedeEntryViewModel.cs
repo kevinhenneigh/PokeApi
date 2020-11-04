54b31c9c45bea339f131635395579e22a123b284
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-11-03 19:59:46 -0800

    Added image to Pokedex view model

--- PokeAPIWebsite/Models/PokemonPokedeEntryViewModel.cs
+++ PokeAPIWebsite/Models/PokemonPokedeEntryViewModel.cs
@@ -5,8 +5,13 @@ using System.Threading.Tasks;
 
 namespace PokeAPIWebsite.Models
 {
+    /// <summary>
+    /// Information for a single Pokemon Pokedex
+    /// </summary>
     public class PokemonPokedeEntryViewModel
     {
+        public string PokedexImageUrl { get; set; }
+
         public string Name { get; set; }
 
         public int Id { get; set; }
